3b120ac8c181c45a476954c6d5b3197f1f1b0281
Ricky <ricky@ricky.com>
2022-12-03 16:43:58 -0600

    figured out add alert portion

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-
+    let form = document.getElementById("launchForm");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        let pilotName = document.querySelector('[name="pilotName"]').value;
+        let copilotName = document.querySelector('[name="copilotName"]').value;
+        let fuelLevel = document.querySelector('[name="fuelLevel"]').value;
+        let cargoMass = document.querySelector('[name="cargoMass"]').value;
+        let list = document.getElementById('faultyItems');
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+    })
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -12,5 +21,5 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
 });
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,37 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if(testInput === ''){
+        return "Empty";
+    } else if(isNaN(Number(testInput))){
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let arr = [pilot, copilot, fuelLevel, cargoLevel];
+    let arrResults = [];
+    for(let i=0; i<arr.length; i++){
+        arrResults.push(validateInput(arr[i]));
+    }
+    console.log(arrResults);
+    for(let j=0; j<arrResults.length; j++){
+        if(arrResults[j] === 'Empty'){
+            alert("All entries must be filled out");
+        }
+        if(j === 0 || j === 1){
+            if(arrResults[j] === "Is a Number"){
+                alert("Enter valid name for pilot and copilot");
+            }
+        } else {
+            if(arrResults[j] === "Not a Number"){
+                alert("Enter valid number for Fuel Level and Cargo Leves");
+            }
+        }
+    }
+
 }
 
 async function myFetch() {
